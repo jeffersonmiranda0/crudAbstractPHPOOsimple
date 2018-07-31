7096082086942e37880f3fb25d02b726a201dc0c
jeffersonmiranda0 <jeffersonmiranda0@gmail.com>
2018-07-30 23:44:24 -0300

    UPDATE ADD COMMENT IN INDEX.PHP

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 require_once __DIR__ . '/api/Model/Carro.class.php';
 
 $carro = new Carro();
-$carro->setCodigo("1");
+$carro->setCodigo("1"); //METODO SAVE -> SE ESTE VALOR ESTIVER SETADO ENTÃO REALIZA UPDATE, CASO NAO ESTEJA REALIZA INSERT
 //$carro->setAno('2018');
 //$carro->setMarca('VECTRA123 555d');
 //$carro->setModelo("Expression insert");
 //$carro->setNome('Besouro Negro');
 //$retorno = $carro->save(true, $carro->getNome());
-$retorno = $carro->populateAttr();
+
+$retorno = $carro->populateAttr(); //METODO UTILIZADO PARA CARREGAR NA MEMÓRIA OS VALORES DE 1(UM) REGISTRO
 
 echo $carro->getNome() . '<br />';
 
