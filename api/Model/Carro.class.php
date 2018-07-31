bb9bb6eaaa74c11496ad3cb9224f21d763ca69c4
jeffersonmiranda0 <jeffersonmiranda0@gmail.com>
2018-07-31 01:09:51 -0300

    init dev read annotation

--- api/Model/Carro.class.php
+++ api/Model/Carro.class.php
@@ -10,56 +10,109 @@ class Carro extends AbstractModel{
 
     protected $ignore       = Array();
 
-    protected static $config       = Array(
+    protected $config       = Array(
         "createTable" => true
     );
-    
+
+    /**
+     * @var int $codigo
+     */
     protected $codigo;
+    /**
+     * @var string $nome;
+     */
     protected $nome;
+    /**
+     * @var string $marca
+     */
     protected $marca;
+    /**
+     * @var string $modelo
+     */
     protected $modelo;
+    /**
+     * @var int $ano
+     */
     protected $ano;
 
+
+    /**
+     * @return int
+     */
     public function getCodigo()
     {
         return $this->codigo;
     }
 
+    /**
+     * @param int $codigo
+     */
     public function setCodigo($codigo)
     {
         $this->codigo = $codigo;
     }
 
-    public function getNome() {
+    /**
+     * @return string
+     */
+    public function getNome()
+    {
         return $this->nome;
     }
 
-    public function getMarca() {
-        return $this->marca;
-    }
-
-    public function getModelo() {
-        return $this->modelo;
-    }
-
-    public function getAno() {
-        return (int)$this->ano;
-    }
-
-    public function setNome($nome) {
+    /**
+     * @param string $nome
+     */
+    public function setNome($nome)
+    {
         $this->nome = $nome;
     }
 
-    public function setMarca($marca) {
+    /**
+     * @return string
+     */
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * @param string $marca
+     */
+    public function setMarca($marca)
+    {
         $this->marca = $marca;
     }
 
-    public function setModelo($modelo) {
+    /**
+     * @return string
+     */
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    /**
+     * @param string $modelo
+     */
+    public function setModelo($modelo)
+    {
         $this->modelo = $modelo;
     }
 
-    public function setAno($ano) {
-        $this->ano = (int)$ano;
+    /**
+     * @return int
+     */
+    public function getAno()
+    {
+        return $this->ano;
     }
-    
+
+    /**
+     * @param int $ano
+     */
+    public function setAno($ano)
+    {
+        $this->ano = $ano;
+    }
 }
--- example.php
+++ example.php
@@ -1,3 +1,10 @@
 <?php
-/**{@Schema : Pessoa}*/
-/**{@Table : test}*/
+
+
+/**
+ * @var int $codigo;
+ * @column(codigo int not null auto_increment primary key);
+ */
+
+//abc
+
